5835d62a10e33e0c99662bd0f77b95325e8a113b
Rod Carey <rj.carey@comcast.net>
2024-09-19 13:42:15 -0400

    Day 27: send email added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
+from send_email import send_email
 
 api_key = "5f40975c91644632b2dc7d5000c5d9ea"
-url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5f40975c91644632b2dc7d5000c5d9ea"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2024-08-19&sortBy=publishedAt&apiKey=5f40975c91644632b2dc7d5000c5d9ea"
 
 # Make request
 request = requests.get(url)
@@ -9,7 +10,12 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-# Access the article titles and descritions
+# Access the article titles and descriptions
+body = ""
 for article in content["articles"]:
-   print(article["title"])
-   print(article["description"])
+   if article["title"] is not None:
+       body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+
+
+body = body.encode("utf-8")
+send_email(message=body)
